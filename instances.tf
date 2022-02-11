947ddc9bbfe1e48c270f5d5e6949940e7e3b713b
Nick <marshallnw18@gmail.com>
2022-02-11 10:26:25 -0500

    Creating and bootstrapping Jenkins nodes

--- instances.tf
+++ instances.tf
@@ -12,14 +12,54 @@ data "aws_ssm_parameter" "linuxAmiOregon" {
 
 # Attach the public key from the user's "~/.ssh/id_rsa.pub" file to the Jenkins master node for keypair login
 resource "aws_key_pair" "master-key" {
-  provider = aws.region-master
-  key_name = "jenkins"
+  provider   = aws.region-master
+  key_name   = "jenkins"
   public_key = file("~/.ssh/id_rsa.pub")
 }
 
 # Attach the public key from the user's "~/.ssh/id_rsa.pub" file to the Jenkins worker node for keypair login
 resource "aws_key_pair" "worker-key" {
-  provider = aws.region-worker
-  key_name = "jenkins"
+  provider   = aws.region-worker
+  key_name   = "jenkins"
   public_key = file("~/.ssh/id_rsa.pub")
+}
+
+# Create and boostrap Jenkins Master Node in us-east-1
+resource "aws_instance" "jenkins-master" {
+  provider                    = aws.region-master
+  ami                         = data.aws_ssm_parameter.linuxAmi.value
+  instance_type               = var.instance-type
+  key_name                    = aws_key_pair.master-key.key_name
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.jenkins-sg.id]
+  subnet_id                   = aws_subnet.subnet_1.id
+
+  tags = {
+    Name = "jenkins_master_tf"
+  }
+
+  depends_on = [
+    aws_main_route_table_association.set-master-default-rt-assoc
+  ]
+}
+
+# Create and bootstrap Jenkins Worker Nodes in us-west-2
+resource "aws_instance" "jenkins-worker-oregon" {
+  provider                    = aws.region-worker
+  count                       = var.workers-count
+  ami                         = data.aws_ssm_parameter.linuxAmiOregon.value
+  instance_type               = var.instance-type
+  key_name                    = aws_key_pair.worker-key.key_name
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.jenkins-sg-oregon.id]
+  subnet_id                   = aws_subnet.subnet_1_oregon.id
+
+  tags = {
+    Name = join("_", ["jenkins_worker_tf", count.index + 1])
+  }
+
+  depends_on = [
+    aws_main_route_table_association.set-master-default-rt-assoc,
+    aws_instance.jenkins-master
+  ]
 }
